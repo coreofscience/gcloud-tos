5756496fc1c211c172a122cecc5d21b999fab48a
Juan David Alzate Cardona <jdalzatec@unal.edu.co>
2020-10-11 16:33:30 -0500

    Build ToS json structure from a set of stored files in the gcloud storage

--- create-tree/main.py
+++ create-tree/main.py
@@ -1,12 +1,39 @@
 import os
+from io import StringIO
 from typing import Dict, List
+
 from google.cloud import storage
+from igraph import Graph
+from sap import CachedCollection, Sap, giant
 
 storage_client = storage.Client()
 
 
-def tree_from_strings(strings: List[str]) -> Dict[str, List[Dict]]:
-    return {}
+def tree_from_strings(strings: List[str]) -> Graph:
+    """Creates a ToS tree from a list of strings."""
+    sap = Sap()
+    graph = giant(CachedCollection(*[StringIO(text) for text in strings]))
+    return sap.tree(graph)
+
+
+def convert_tos_to_json(tree: Graph) -> Dict[str, List[Dict]]:
+    """Converts a ToS graph in the default format to be processed by the
+    frontend. The default format is like:
+    {
+        "root": [{"label": "...", "title": "...", "authors": "...", ...}, ...],
+        "trunk": [{"label": "...", "title": "...", "authors": "...", ...}, ...],
+        "leaf": [{"label": "...", "title": "...", "authors": "...", ...}, ...],
+    }
+    """
+    output = {}
+
+    labels = ["root", "trunk", "leaf"]
+    for label in labels:
+        vertices = tree.vs.select(**{f"{label}_gt": 0})
+        data = [vertex.attributes() for vertex in vertices]
+        output[label] = data
+
+    return output
 
 
 def create_tree(event, context):
@@ -20,12 +47,9 @@ def create_tree(event, context):
     blobs = [bucket.get_blob(name) for name in names]
     print(blobs)
     contents = [blob.download_as_text() for blob in blobs if blob is not None]
-    print(contents)
 
-    for content in contents:
-        print(content[:1000])
+    tos = tree_from_strings(contents)
+    result = convert_tos_to_json(tos)
 
-
-if __name__ == "__main__":
-    result = tree_from_strings([])
     print(result)
+    return result
