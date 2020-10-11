a98566493624b8ce31e6b5aa9886a0a90aa8c99d
Oscar Arbeláez <odarbelaeze@gmail.com>
2020-10-11 16:01:59 -0500

    Scafold main function

--- create-tree/main.py
+++ create-tree/main.py
@@ -1,7 +1,12 @@
 import os
+from typing import Dict, List
 from google.cloud import storage
 
 storage_client = storage.Client()
+
+
+def tree_from_strings(strings: List[str]) -> Dict[str, List[Dict]]:
+    return {}
 
 
 def create_tree(event, context):
@@ -19,3 +24,8 @@ def create_tree(event, context):
 
     for content in contents:
         print(content[:1000])
+
+
+if __name__ == "__main__":
+    result = tree_from_strings([])
+    print(result)
