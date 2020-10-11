670b57888e985d50205b55af886f1586be714c46
Oscar Arbeláez <odarbelaeze@gmail.com>
2020-10-11 15:56:18 -0500

    Get the files themselves

--- create-tree/main.py
+++ create-tree/main.py
@@ -1,16 +1,21 @@
-import json
+import os
+from google.cloud import storage
+
+storage_client = storage.Client()
+
 
 def create_tree(event, context):
+    print(event, context)
+    delta = event.get("delta", {"files": {}})
+    print(delta)
+    bucket_name = os.getenv("STORAGEBUCKET")
+    bucket = storage_client.get_bucket(bucket_name)
+    names = [f"isi-files/{name}" for name in delta["files"].values()]
+    print(names)
+    blobs = [bucket.get_blob(name) for name in names]
+    print(blobs)
+    contents = [blob.download_as_text() for blob in blobs if blob is not None]
+    print(contents)
 
-    print('event', event)
-    print('context', context)
-
-    if 'params' in event:
-        print('Path parameters:')
-        for param, value in event['params'].items():
-            print(f'  {param}: {value}')
-
-    print('Function triggered by change to:',  context.resource)
-    print('Admin?: %s' % event.get("admin", False))
-    print('Delta:')
-    print(json.dumps(event["delta"]))
+    for content in contents:
+        print(content[:1000])
--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ gcloud functions deploy create_tree \
   --project coreofscience-dev \
   --trigger-event providers/google.firebase.database/eventTypes/ref.create \
   --trigger-resource projects/_/instances/coreofscience-dev/refs/trees/{treeId} \
+  --set-env-vars STORAGEBUCKET="coreofscience-dev.appspot.com" \
   --source ./create-tree \
   --runtime python38
